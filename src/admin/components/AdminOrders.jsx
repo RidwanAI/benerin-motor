905062c3597f7303d66771a9302c24ee95816524
Muhammad Ridwan <ridwansmpl36@gmail.com>
2025-01-23 00:16:44 +0700

    Add Delete & View Orders

--- src/admin/components/AdminOrders.jsx
+++ src/admin/components/AdminOrders.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from "react";
+import { Eye } from "lucide-react";
 import adminService from "../../services/adminService";
 
 const AdminOrders = () => {
-  /*
-    Function Order -> Create, Read, Update, Delete
-  */
   const [orders, setOrders] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [loading, setLoading] = useState(true);
@@ -18,10 +16,9 @@ const AdminOrders = () => {
   useEffect(() => {
     const fetchOrders = async () => {
       try {
-        const data = await adminService.getOrders(); // Fetch orders from your API
+        const data = await adminService.getOrders();
         setOrders(data);
       } catch (err) {
-        console.error("Error fetching orders:", err);
         setError("Failed to fetch orders.");
       } finally {
         setLoading(false);
@@ -30,6 +27,22 @@ const AdminOrders = () => {
 
     fetchOrders();
   }, []);
+
+  const deleteOrder = async (orderId) => {
+    if (window.confirm("Are you sure you want to delete this order?")) {
+      try {
+        await adminService.deleteOrder(orderId);
+        setOrders(orders.filter((order) => order.id !== orderId));
+        alert("Order deleted successfully!");
+      } catch (err) {
+        alert("Failed to delete order.");
+      }
+    }
+  };
+
+  const openImageInNewTab = (imageUrl) => {
+    window.open(imageUrl, '_blank');
+  };
 
   if (loading) return <p>Loading orders...</p>;
   if (error) return <p className="text-red-500">{error}</p>;
@@ -51,7 +64,11 @@ const AdminOrders = () => {
 
     try {
       await adminService.updateOrder(selectedOrder.id, { status });
-      setOrders((prevOrders) => prevOrders.map((order) => (order.id === selectedOrder.id ? { ...order, status } : order)));
+      setOrders((prevOrders) =>
+        prevOrders.map((order) =>
+          order.id === selectedOrder.id ? { ...order, status } : order
+        )
+      );
       closeModal();
       alert("Order status updated successfully!");
     } catch (err) {
@@ -60,19 +77,19 @@ const AdminOrders = () => {
     }
   };
 
-  /* 
-    Function -> Searching
-  */
   const filteredOrders = orders.filter(
-    (order) => order.user?.name.toLowerCase().includes(searchQuery.toLowerCase()) || order.status.toLowerCase().includes(searchQuery.toLowerCase()) || order.shippingAddress.toLowerCase().includes(searchQuery.toLowerCase())
+    (order) =>
+      order.user?.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      order.status.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      order.shippingAddress.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
-  /* 
-    Function -> Pagination
-  */
   const totalPages = Math.ceil(filteredOrders.length / itemsPerPage);
   const startIndex = (currentPage - 1) * itemsPerPage;
-  const currentOrders = filteredOrders.slice(startIndex, startIndex + itemsPerPage);
+  const currentOrders = filteredOrders.slice(
+    startIndex,
+    startIndex + itemsPerPage
+  );
 
   const goToPreviousPage = () => {
     setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
@@ -84,15 +101,12 @@ const AdminOrders = () => {
 
   return (
     <div className="flex flex-col font-poppins min-h-screen">
-      {/* Header */}
       <div className="bg-slate-100 flex items-center p-3 shadow-sm sticky top-0 z-30">
         <p className="font-bold text-2xl">Orders Data</p>
       </div>
 
-      {/* Main Content */}
       <div className="flex-1 bg-slate-100 overflow-auto">
         <main className="p-3 space-y-3">
-          {/* Fitur -> Search */}
           <input
             type="text"
             placeholder="Search by Customer Name, Status, or Shipping Address"
@@ -104,7 +118,6 @@ const AdminOrders = () => {
             className="border p-2 rounded-sm w-full"
           />
 
-          {/* Fitur -> Table Order */}
           <div className="overflow-auto">
             <table className="bg-white border-collapse overflow-hidden rounded-sm shadow-sm w-full">
               <thead className="bg-gray-800 text-white">
@@ -129,13 +142,18 @@ const AdminOrders = () => {
                     <td className="px-4 py-3">{order.user?.name || "Unknown User"}</td>
                     <td className="px-4 py-3">{order.orderedProduct?.name || "Unknown Product"}</td>
                     <td className="px-4 py-3">{order.quantity}</td>
-                    <td className="px-4 py-3">{`Rp.${parseFloat(order.totalPrice).toLocaleString("id-ID", { minimumFractionDigits: 2 })}`}</td>
+                    <td className="px-4 py-3">{`Rp.${parseFloat(order.totalPrice).toLocaleString("id-ID", {
+                      minimumFractionDigits: 2,
+                    })}`}</td>
                     <td className="px-4 py-3">{order.status}</td>
                     <td className="px-4 py-3">
                       {order.paymentProof ? (
-                        <a href={order.paymentProof} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
-                          View
-                        </a>
+                        <button
+                          onClick={() => openImageInNewTab(order.paymentProof)}
+                          className="text-blue-500 hover:text-blue-700"
+                        >
+                          <Eye size={20} />
+                        </button>
                       ) : (
                         "None"
                       )}
@@ -143,9 +161,18 @@ const AdminOrders = () => {
                     <td className="px-4 py-3">{order.shippingAddress}</td>
                     <td className="px-4 py-3">{order.customerPhoneNumber}</td>
                     <td className="px-4 py-3">{order.shippingMethod}</td>
-                    <td className="px-4 py-3">
-                      <button onClick={() => openModal(order)} className="bg-yellow-500 px-3 py-1.5 text-white rounded hover:bg-yellow-600">
+                    <td className="px-4 py-3 space-x-2">
+                      <button
+                        onClick={() => openModal(order)}
+                        className="bg-yellow-500 px-3 py-1.5 text-white rounded hover:bg-yellow-600"
+                      >
                         Edit Status
+                      </button>
+                      <button
+                        onClick={() => deleteOrder(order.id)}
+                        className="bg-red-500 px-3 py-1.5 text-white rounded hover:bg-red-600"
+                      >
+                        Delete
                       </button>
                     </td>
                   </tr>
@@ -161,28 +188,40 @@ const AdminOrders = () => {
             </table>
           </div>
 
-          {/* Fitur -> Pagination */}
           <div className="flex items-center justify-between">
-            <button onClick={goToPreviousPage} disabled={currentPage === 1} className="bg-orange-500 duration-300 px-3 py-1.5 rounded-md transition text-sm text-white disabled:opacity-50 hover:bg-orange-700">
+            <button
+              onClick={goToPreviousPage}
+              disabled={currentPage === 1}
+              className="bg-orange-500 duration-300 px-3 py-1.5 rounded-md transition text-sm text-white disabled:opacity-50 hover:bg-orange-700"
+            >
               Previous
             </button>
             <span>
               Page {currentPage} of {totalPages}
             </span>
-            <button onClick={goToNextPage} disabled={currentPage === totalPages} className="bg-orange-500 duration-300 px-3 py-1.5 rounded-md transition text-sm text-white disabled:opacity-50 hover:bg-orange-700">
+            <button
+              onClick={goToNextPage}
+              disabled={currentPage === totalPages}
+              className="bg-orange-500 duration-300 px-3 py-1.5 rounded-md transition text-sm text-white disabled:opacity-50 hover:bg-orange-700"
+            >
               Next
             </button>
           </div>
         </main>
 
-        {/* Fitur -> Pop Up Edit Status Order */}
         {isModalOpen && selectedOrder && (
           <div className="bg-slate-500 bg-opacity-50 fixed inset-0 flex items-center justify-center px-3 z-50">
             <div className="bg-white p-3 rounded-sm space-y-3 w-full md:w-1/3">
               <p className="text-xl font-semibold">Edit Order Status</p>
               <div>
-                <label className="block mb-2 text-sm font-medium">Order ID: {selectedOrder.id}</label>
-                <select value={status} onChange={(e) => setStatus(e.target.value)} className="w-full p-2 border rounded">
+                <label className="block mb-2 text-sm font-medium">
+                  Order ID: {selectedOrder.id}
+                </label>
+                <select
+                  value={status}
+                  onChange={(e) => setStatus(e.target.value)}
+                  className="w-full p-2 border rounded"
+                >
                   <option value="Pending">Pending</option>
                   <option value="Paid">Paid</option>
                   <option value="Shipped">Shipped</option>
@@ -190,10 +229,16 @@ const AdminOrders = () => {
                 </select>
               </div>
               <div className="flex justify-end gap-3">
-                <button onClick={closeModal} className="bg-gray-300 px-4 py-2 rounded hover:bg-gray-500">
+                <button
+                  onClick={closeModal}
+                  className="bg-gray-300 px-4 py-2 rounded hover:bg-gray-500"
+                >
                   Cancel
                 </button>
-                <button onClick={updateOrderStatus} className="bg-orange-500 px-4 py-2 text-white rounded hover:bg-orange-700">
+                <button
+                  onClick={updateOrderStatus}
+                  className="bg-orange-500 px-4 py-2 text-white rounded hover:bg-orange-700"
+                >
                   Save
                 </button>
               </div>
